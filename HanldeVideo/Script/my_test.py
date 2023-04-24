f17cadc9808380c7b09f1509b632e6f0069c3f33
Walker <luowenkuo@hotmail.com>
2023-04-24 22:19:19 +0800

    0424

--- HanldeVideo/Script/my_test.py
+++ HanldeVideo/Script/my_test.py
@@ -1,6 +1,7 @@
-def txt_pcd(string):
-    print('hello world' + string)
+import open3d as o3d
+import numpy as np
 
-
-# if __name__ == "__main__":
-#     txt_pcd("ni")
+pcd = o3d.io.read_point_cloud("../test.pcd")
+coor = o3d.geometry.TriangleMesh.create_coordinate_frame(size=100,
+                                                         origin=[0, 0, 0])
+o3d.visualization.draw_geometries([coor, pcd], width=500, height=500)
