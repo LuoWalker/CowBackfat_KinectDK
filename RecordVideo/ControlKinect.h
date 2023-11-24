ab0ebd9d3b9959260f66227f0de399a77b8e89c9
Walker <luowenkuo@hotmail.com>
2023-11-24 18:51:19 +0800

    删除官方录制器判断头帧的操作而直接录制

---
+++ RecordVideo/ControlKinect.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <k4a/k4a.h>
+#include <atomic>
+
+extern std::atomic_bool exiting;
+
+class Kinect
+{
+public:
+	Kinect();
+	k4a_result_t isOpen();
+	void startRecord(k4a_device_configuration_t config);
+
+	int doRecording(uint8_t device_index,
+		char *recording_filename,
+		int recording_length,
+		k4a_device_configuration_t *device_config,
+		bool record_imu,
+		int32_t absoluteExposureValue,
+		int32_t gain);
+
+private:
+	k4a_device_t device = NULL;
+	k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+	static const int32_t defaultExposureAuto = -12;
+	static const int32_t defaultGainAuto = -1;
+
+};
--- RecordVideo/main.cpp
+++ RecordVideo/main.cpp
@@ -1,42 +1,17 @@
-#include <iostream>
-#include <sstream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/video.hpp>
-#include <opencv2/bgsegm.hpp>
-using namespace cv;
-using namespace std;
+#include "ControlKinect.h"
+#include <k4a/k4a.h>
 
 int main(int argc, char *argv[])
 {
-	Ptr<BackgroundSubtractor> pBackSub = bgsegm::createBackgroundSubtractorCNT();
-	VideoCapture capture("F:/luowenkuo/Video/0513/05131.mkv");
-
-	Mat frame, fgMask;
-	while (true) {
-		capture >> frame;
-		if (frame.empty())
-			break;
-		//update the background model
-		pBackSub->apply(frame, fgMask, -1);
-		//get the frame number and write it on the current frame
-		rectangle(frame, cv::Point(10, 2), cv::Point(100, 20),
-			cv::Scalar(255, 255, 255), -1);
-		stringstream ss;
-		ss << capture.get(CAP_PROP_POS_FRAMES);
-		string frameNumberString = ss.str();
-		putText(frame, frameNumberString.c_str(), cv::Point(15, 15),
-			FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0));
-		//show the current frame and the fg masks
-		imshow("Frame", frame);
-		imshow("FG Mask", fgMask);
-		//get the input from the keyboard
-		int keyboard = waitKey(30);
-		if (keyboard == 'q' || keyboard == 27)
-			break;
+	Kinect kinect;
+	/*if (K4A_RESULT_SUCCEEDED == kinect.isOpen())*/ {
+		// Configure a stream of 4096x3072 BRGA color data at 15 frames per second
+		k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+		config.camera_fps = K4A_FRAMES_PER_SECOND_30;
+		config.color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32;
+		config.color_resolution = K4A_COLOR_RESOLUTION_1080P;
+		config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
+		kinect.startRecord(config);
 	}
-
 	return 0;
 }
